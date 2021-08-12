949f3cef4fb84bd5b451c3db81d2ed1da148521b
Jon Howell <howell@vmware.com>
2021-08-12 11:15:24 -0700

    normalize: docker stuff lives on instructor repo

--- tools/dafny-server-docker/build-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Building docker images
-#https://www.linode.com/docs/kubernetes/deploy-container-image-to-kubernetes/
-#https://training.play-with-docker.com/beginner-linux/#Task_2
-
-docker image build --tag jonhdotnet/summer_school:1.1 dafny_server/
-# docker login
-# docker push jonhdotnet/summer_school:1.1
--- tools/dafny-server-docker/dafny_server/install-dot-net.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# pulled this into a separate script so we could use /etc/os-release to get release info
-. /etc/os-release
-wget https://packages.microsoft.com/config/${ID}/${VERSION_ID}/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
-apt-get update
-apt-get install -y dotnet-sdk-5.0
